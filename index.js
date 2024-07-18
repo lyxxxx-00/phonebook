49e481ae1892d9cae42b5e7aba32e3337daa1848
lyxxxx-00 <lyixuan5555@gmail.com>
2024-07-18 20:43:02 +0100

    upload frontend

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 app.use(express.json());
 
 app.use(cors());
-
+app.use(express.static("build"));
 morgan.token("body", (req, res) =>
   req.method === "POST" ? JSON.stringify(req.body) : ""
 );
